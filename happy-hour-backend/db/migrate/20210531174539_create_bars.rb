5bfe6c2cbfa8e99958bae764320e2befc7c2f2eb
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-05-31 13:54:36 -0400

    creates bar migration

---
+++ happy-hour-backend/db/migrate/20210531174539_create_bars.rb
@@ -0,0 +1,23 @@
+class CreateBars < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bars do |t|
+      t.string :name
+      t.datetime: :verified
+      t.boolean :sunday
+      t.boolean :monday
+      t.boolean :tuesday
+      t.boolean :wednesday
+      t.boolean :thursday
+      t.boolean :friday
+      t.boolean :saturday
+      t.datetime :start
+      t.datetime :end
+      t.text :details
+      t.integer :phone
+      t.string :website
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
