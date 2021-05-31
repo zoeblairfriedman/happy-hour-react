f4f8955f157c9bbc76307f5627bea37424779c0e
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-05-31 16:20:04 -0400

    sets up rsession controller

--- happy-hour-backend/app/controllers/api/sessions_controller.rb
+++ happy-hour-backend/app/controllers/api/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
 
     def create
-    end
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          render json: @user
+        else
+          @errors = @user.errors.full_messages
+          render json: @errors
+        end
+      end
 
     def destroy
+        session.clear
+        render json: {message: "Sucessfully logged out"}
     end
-    
+
 end
