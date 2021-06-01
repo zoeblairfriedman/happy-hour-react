9d0d34872533df599652754bfb9650c399c3eeba
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-06-01 09:02:58 -0400

    creates store

--- happy-hour-frontend/src/index.js
+++ happy-hour-frontend/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
-// const store = createStore
+// const store = createStore(reducer, applyMiddleware(thunk))
+// let reducer = ""
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+    </Provider>,
   document.getElementById('root')
 );
 
