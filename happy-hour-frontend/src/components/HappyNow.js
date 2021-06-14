4a03234731ab9592addc4bdff38cd63fa58ac5f1
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-06-14 15:01:16 -0400

    adds alphabetical sorting button

--- happy-hour-frontend/src/components/HappyNow.js
+++ happy-hour-frontend/src/components/HappyNow.js
@@ -1,16 +1,27 @@
 import React from 'react'
-
+import Bars from './Bars'
+import {useSelector} from 'react-redux'
+import {useState} from 'react'
 
 
 const HappyNow = () => {
 
+const bars = useSelector(state => state.bars)
+const [sortedBars, setBars] = useState(bars)
+
+function handleClick(){
+    setBars(bars.sort((a,b) => (a.name > b.name) ? 1 : -1))
+}
 
             return (
                 <div>
-                    <p>Coming Soon!</p>
+                    <p>Coming Soon! For now, please enjoy this button:</p>
+                    <button onClick={() => handleClick()}> Sort!</button>
+                    <Bars bars={bars}/>
                 </div>
             )
 
  }
 
 export default HappyNow;  
+
