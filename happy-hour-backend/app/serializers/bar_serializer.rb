694ab2c50b3117b978a7debfb292fc394590671a
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-07-30 15:06:24 -0400

    adds individual days into serializer

--- happy-hour-backend/app/serializers/bar_serializer.rb
+++ happy-hour-backend/app/serializers/bar_serializer.rb
@@ -1,5 +1,5 @@
 class BarSerializer < ActiveModel::Serializer
-    attributes :id, :name, :military_start, :military_end, :date_array, :formatted_verified, :formatted_start, :formatted_end, :details, :phone, :address, :website, :image, :place_id, :lat, :lng, :favorites
+    attributes :id, :name, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, :military_start, :military_end, :date_array, :formatted_verified, :formatted_start, :formatted_end, :details, :phone, :address, :website, :image, :place_id, :lat, :lng, :favorites
   
   def formatted_verified
     object.verified ? object.verified.strftime("%B %d, %Y") : nil
